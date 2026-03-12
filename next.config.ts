12163272a7816be85d228d4e51e2e0bae4605603
charles <osehaeguale@gmail.com>
2026-03-12 23:21:54 +0100

    update proxy issues

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         source: "/__/auth/:path*",
         destination: "https://boo-ik.firebaseapp.com/__/auth/:path*",
       },
+      {
+        source: "/api/proxy/:path*",
+        destination: `${process.env.API_PROXY_TARGET || "http://localhost:3002"}/:path*`,
+      },
     ];
   },
 };
